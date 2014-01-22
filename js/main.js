1b3671d64c384e9a8a97e50d4154618eaf2b8673
A40159 <aimluck.ippei.takahashi@gmail.com>
2014-01-22 19:30:13 +0900

    dialog

--- js/main.js
+++ js/main.js
@@ -15,12 +15,20 @@ Builder.prototype.init = function() {
 	var self = this;
 	this.draggableSelector = "#imgRow ul li";
 	this.sortableSelector = "#imgRow ul";
+	this.draggableImages = $("#imgRow ul li div img");
+
 	this.overlay = $("#overlay");
 	this.wrapper = $("#wrapper");
+	this.range = $("#range");
 	this.timeline = $($("#main table tbody tr")[1]);
+	this.dialog = $("#dialog");
+	this.dialogImg = $("#dialog img")[0];
+	this.dialogOpened = false;
 
 	this.initDraggable();
 	this.initSortable();
+	this.initDialog();
+
 	this.resizeOverlay();
 	$(window).resize(function(){
 		self.resizeOverlay();
@@ -65,24 +73,24 @@ Builder.prototype.sortHandler = function(item) {
 	if($(".sortable-placeholder").children().length == 0) {
 		$(".sortable-placeholder").append($(item.children()[0]).clone());
 	}
-}
+};
 
 Builder.prototype.startHandler = function(item) {
 	item.css("display", "none");
-}
+};
 
 Builder.prototype.stopHandler = function(item) {
 	item.css("position", "relative");
 	item.css("display", "block");
 	item.css("z-index", "1000");
 	item.removeClass("ui-draggable-dragging ui-sortable-helper");
-}
+};
 
 Builder.prototype.resizeOverlay = function() {
 	this.overlay.attr("height", this.wrapper.height());
 	this.overlay.attr("width", this.wrapper.width());
 	this.drawOverlay();
-}
+};
 
 Builder.prototype.drawOverlay = function() {
 	if (!this.overlay || !overlay.getContext) {
@@ -108,5 +116,38 @@ Builder.prototype.drawOverlay = function() {
 
 	ctx.closePath();
 	ctx.stroke();
-}
+};
 
+Builder.prototype.initDialog = function() {
+	var self = this;
+	this.draggableImages.on("click", function(){
+		self.openDialog(this);
+	});
+	this.overlay.on("click", function() {
+		self.closeDialog();
+	});
+};
+
+Builder.prototype.initScroll = function() {
+	this.dialog[0].onscroll = function() {
+		var scroll = this.scrollTop;
+		var range = this.scrollHeight - this.offsetHeight;
+		console.log(scroll);
+		console.log(range);
+	}
+};
+
+Builder.prototype.openDialog = function(img) {
+	if(!this.dialogOpened) {
+		this.dialogOpened = true;
+		this.dialog.css("display", "");
+		this.dialogImg.src = img.src;		
+	}
+};
+
+Builder.prototype.closeDialog = function() {
+	if(this.dialogOpened) {
+		this.dialogOpened = false;
+		this.dialog.css("display", "none");
+	}
+};
